1b55db6253d1143bdcf1ddf2d43519ba6d176b02
pecuchet <hello@dotburo.com>
2017-07-08 15:13:11 +0200

    tmp fix colorspace on frame buffer

--- render/framebuffer.py
+++ render/framebuffer.py
@@ -1,22 +1,29 @@
+import cv2
 import pygame
 import numpy
 
 
 class PyGameRender:
 
-    def __init__(self, screen_size):
+    def __init__(self, resolution=None):
         # os.putenv('SDL_VIDEODRIVER', 'directfb')
         pygame.display.init()
 
-        if screen_size:
-            self.screen_size = screen_size
+        if resolution:
+            self.screen_size = resolution
         else:
             self.screen_size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
 
-        self.screen = pygame.display.set_mode(self.screen_size, -2147483648)
+        # see https://www.pygame.org/docs/ref/display.html
+        # opengl, fullscreen ... options
+        self.screen = pygame.display.set_mode(self.screen_size, 0, 0) #  -2147483648
         self.surface = pygame.Surface(self.screen.get_size())
 
     def render(self, cv_image):
+        # todo can this be somewhere else?
+        # todo do we really need all conversions BGR > HSV > BGR > RGB ...
+        # todo probably better to convert config HSV colours to RGB
+        cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
         image = numpy.rot90(cv_image)
 
         # frame = pygame.surfarray.make_surface(frame)
